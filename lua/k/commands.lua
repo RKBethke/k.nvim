5941a9164934fb5c9397c564bd63ca95f7e41b00
RKBethke <ryanbethke11@gmail.com>
2024-03-31 06:35:52 +0000

    feat(commands): Add visual mode support: eval_selection

--- lua/k/commands.lua
+++ lua/k/commands.lua
@@ -5,13 +5,50 @@ local va = vim.api
 -- Locals
 
 local function get_visual_lines()
-	local vstart = vim.fn.getpos("'<")
-	local vend = vim.fn.getpos("'>")
-	return va.nvim_buf_get_lines(0, vstart[2], vend[2])
+	local cursor = vim.api.nvim_win_get_cursor(0)
+	local cline, ccol = cursor[1], cursor[2]
+	local vline, vcol = vim.fn.line("v"), vim.fn.col("v")
+
+	local sline, scol
+	local eline, ecol
+	if cline == vline then
+		if ccol <= vcol then
+			sline, scol = cline, ccol
+			eline, ecol = vline, vcol
+			scol = scol + 1
+		else
+			sline, scol = vline, vcol
+			eline, ecol = cline, ccol
+			ecol = ecol + 1
+		end
+	elseif cline < vline then
+		sline, scol = cline, ccol
+		eline, ecol = vline, vcol
+		scol = scol + 1
+	else
+		sline, scol = vline, vcol
+		eline, ecol = cline, ccol
+		ecol = ecol + 1
+	end
+
+	return va.nvim_buf_get_lines(0, sline - 1, eline, 0)
 end
 
 local function get_buffer_lines()
 	return va.nvim_buf_get_lines(0, 0, -1, true)
+end
+
+local function run_k(cmd)
+	local origdir = vim.loop.cwd()
+	local bufdir = vim.fn.expand("%:p:h")
+
+	vim.cmd.cd(bufdir)
+	local p = assert(io.popen(cmd))
+	local output = p:read("*all")
+	p:close()
+	vim.cmd.cd(origdir)
+
+	return vim.split(output, "\n")
 end
 
 -- Module
@@ -20,25 +57,30 @@ function M.eval(opts)
 	local config = require("k.config")
 	table.insert(config, opts or {})
 
-	local origdir = vim.loop.cwd()
-	local bufdir = vim.fn.expand("%:p:h")
-
 	local lines = get_buffer_lines()
 	if not lines then
 		return
 	end
 	local program = table.concat(lines, "\n")
-
 	local cmd = 'printf "' .. program .. '\n" | ' .. config.path .. " 2>&1"
+	require("k.postwin").post(run_k(cmd))
+end
 
-	vim.cmd.cd(bufdir)
-	local p = assert(io.popen(cmd))
-	local output = p:read("*all")
-	p:close()
-	vim.cmd.cd(origdir)
+function M.eval_selection(opts)
+	local config = require("k.config")
+	table.insert(config, opts or {})
 
-	local out_lines = vim.split(output, "\n")
-	require("k.postwin").post({ unpack(out_lines, 1, #out_lines - 1) })
+	local lines = get_visual_lines()
+	if not lines then
+		return
+	end
+	local program = table.concat(lines, "\n")
+	local cmd = 'printf "' .. program .. '\n" | ' .. config.path .. " 2>&1"
+	require("k.postwin").post(run_k(cmd))
+end
+
+function M.clear()
+	require("k.postwin").clear()
 end
 
 return M
